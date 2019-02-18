2f4238a968fbc933cc2ed8d95a0d9b139b2c22aa
aMinguezg <44700540+aMinguezg@users.noreply.github.com>
2019-02-18 17:01:27 +0100

    Arreglo Miguel

--- Servicio Peliculas/WS.Rest/WS.Rest.Peliculas/Controllers/PeliculasController.cs
+++ Servicio Peliculas/WS.Rest/WS.Rest.Peliculas/Controllers/PeliculasController.cs
@@ -12,19 +12,20 @@ namespace WS.Rest.Peliculas.Controllers
 {
     public class PeliculasController : ApiController
     {
-        public String Retorno(string titulo)
+        public dynamic Retorno(string id)
         {
-            var client = new RestClient("https://filmaffinity-api.mybluemix.net/api/film/byTitle");
+            var client = new RestClient("https://filmaffinity-api.mybluemix.net/api/film/byId");
             var request = new RestRequest(Method.GET);
-            request.AddParameter("title", titulo);
-            IRestResponse response = client.Execute(request);
-            return response.Content;
+            request.AddParameter("id", id);
+            return client.Execute<dynamic>(request).Data;
         }
 
         public IHttpActionResult Get()
         {
-            var result = Retorno("Troya");
-            return Ok(JsonConvert.DeserializeObject(result));
+            dynamic result = Retorno("/es/film564615.html");
+            //Console.WriteLine(result.result.calificación);
+            //return Ok(JsonConvert.DeserializeObject(result));
+            return Ok(result["result"]["calificación"]);
         }
     }
 }
