732a73220ecfc4a9f81497b438a7a1483719fb98
aMinguezg <44700540+aMinguezg@users.noreply.github.com>
2019-02-19 11:35:50 +0100

    Servicio acabado con Mongo

--- Servicio Peliculas/WS.Rest/WS.Rest.Peliculas/Controllers/PeliculasController.cs
+++ Servicio Peliculas/WS.Rest/WS.Rest.Peliculas/Controllers/PeliculasController.cs
@@ -1,17 +1,15 @@
-﻿using Newtonsoft.Json;
-using RestSharp;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using RestSharp;
 using System.Web.Http;
 using WS.Rest.Peliculas.Models;
+using MongoDB.Driver;
+using System;
 
 namespace WS.Rest.Peliculas.Controllers
 {
     public class PeliculasController : ApiController
     {
+        private MongoClient client = new MongoClient("mongodb://user:user1313@ds341605.mlab.com:41605/peliculasdb");
+        
         public dynamic Retorno(string id)
         {
             var client = new RestClient("https://filmaffinity-api.mybluemix.net/api/film/byId");
@@ -20,11 +18,14 @@ namespace WS.Rest.Peliculas.Controllers
             return client.Execute<dynamic>(request).Data;
         }
 
-        public IHttpActionResult Get()
+        public IHttpActionResult Get(int id)
         {
             Pelicula pelicula = new Pelicula();
-            //Aqui hay que pasar el id de la base de datos
-            dynamic result = Retorno("/es/film564615.html");
+            //Aqui hay que pasar el id a la base de datos
+            IMongoDatabase database = client.GetDatabase("peliculasdb");
+            var collection = database.GetCollection<FindPelicula>("film");
+            var results = collection.Find(Builders<FindPelicula>.Filter.Eq("IdContador", id)).ToList();
+            dynamic result = Retorno(results[0].Url);
 
             //Se rellena el modelo pelicula
             pelicula.Title = result["result"]["Título original"];
@@ -34,7 +35,14 @@ namespace WS.Rest.Peliculas.Controllers
             pelicula.Pais = result["result"]["País"];
             pelicula.Reparto = result["result"]["Reparto"];
             pelicula.Sinopsis = result["result"]["Sinopsis"];
+
             return Ok(pelicula);
+            //Buscar sin tipado
+            //var results = collection.Find(x => x.IdContador == 1).ToList();
+            //Filter con Lambda
+            //var filter = Builders<FindPelicula>.Filter.Eq(x => x.IdContador, 1)
+            //Esto sería Async
+            //var results = await collection.Find(filter).ToListAsync();
         }
 
     }
---
+++ Servicio Peliculas/WS.Rest/WS.Rest.Peliculas/Models/FindPelicula.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WS.Rest.Peliculas.Models
+{
+    public class FindPelicula
+    {
+        public ObjectId Id { get; set; }
+        public int IdContador { get; set; }
+        public string Titulo { get; set; }
+        public string Url { get; set; }
+    }
+}
