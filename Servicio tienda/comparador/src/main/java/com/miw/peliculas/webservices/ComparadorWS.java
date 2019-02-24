499a1dbf0ba17f9a9c5c1bd7056355b5d3f2674e
Mario <mario@fedora-28.shared>
2019-02-24 20:39:15 +0100

    cambios cliente web

--- Servicio tienda/comparador/src/main/java/com/miw/peliculas/webservices/ComparadorWS.java
+++ Servicio tienda/comparador/src/main/java/com/miw/peliculas/webservices/ComparadorWS.java
@@ -1,60 +1,38 @@
 package com.miw.peliculas.webservices;
 
-import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
-import java.util.Set;
 import javax.jws.WebService;
 
+//TODO add support for soap exceptions
+@WebService(endpointInterface = "com.miw.peliculas.webservices.IComparadorWS")
+public class ComparadorWS implements IComparadorWS {
 
-@WebService(endpointInterface="com.miw.peliculas.webservices.IComparadorWS")
-public class ComparadorWS implements IComparadorWS
-{
-	private static List<String> peliculas;
-	
-	public Map<String, Double> comparacion;
-	
-	public enum Comercios
-	{
-		NETFLIX, AMAZON, CARREFOUR, ALCAMPO,
-		TUVIDEOCLUB, FNAC, MEDIAMARKT
+	private List<Comparacion> comparaList;
+
+	public enum Comercio {
+		NETFLIX, AMAZON, CARREFOUR, ALCAMPO, TUVIDEOCLUB, FNAC, MEDIAMARKT
 	}
-	
-	public ComparadorWS()
-	{
-		peliculas = new ArrayList<String>();
-		peliculas.add("El silencio de los corderos");
-		peliculas.add("Star Wars III");
-		peliculas.add("Rocky");
-		peliculas.add("Rocky II");
-		peliculas.add("El señor de los anillos");
-		peliculas.add("El señor de los anillos I");
-		peliculas.add("El señor de los anillos II");
-		peliculas.add(("El señor de los anillos III"));
-	}
-	
-	@Override
-	public String[] comparar(String titulo) {
-		
-		
-		
-		if (peliculas.contains(titulo)) {
-			for (Comercios c: Comercios.values()) {
-				
-			}
+
+	public Comparacion[] comparar() {
+
+		comparaList = new ArrayList<Comparacion>();
+
+		for (Comercio c : Comercio.values()) {
+			comparaList.add(new Comparacion(c.toString(), calcularPrecio()));
 		}
-		return null;
+		Comparacion[] comparaArray = new Comparacion[comparaList.size()];
+		return comparaList.toArray(comparaArray);
+
 	}
-	
-	public String calcularPrecio() {
-		
+
+	//TODO add non-zero decimal part to the calculated prize
+	public double calcularPrecio() {
+
 		Random r = new Random();
-        double randomValue = 5 + (15 - 5) * r.nextDouble();
-        return new DecimalFormat("#.##").format(randomValue);
+		double precio = Math.floor(5 + (15 - 5) * r.nextDouble());
+		return precio;
 	}
-	
 
 }
